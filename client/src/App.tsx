bdffd86fa6ae57841977e9fd064e9366bf36c3ce
Simen H <simenmelo@gmail.com>
2021-09-20 05:09:04 +0200

    fix: Set app element for Modal to #root

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,9 @@ import './App.css';
 
 import NavBar from './components/NavBar/NavBar.component';
 import MyLists from './screens/MyLists/MyLists.screen';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 const App: React.FC = () => {
   return (

f9dc958d2e41a1a3a156f65355a56c78b5486306
Simen H <simenmelo@gmail.com>
2021-09-23 21:47:25 +0200

    feat: Create and import express router

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import router from './router';
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(router);
 
 app.listen(
   PORT,

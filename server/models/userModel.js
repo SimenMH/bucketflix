f22a3a78a4931d16bccf4b827ce7d1b5567dd79d
Simen H <simenmelo@gmail.com>
2021-09-24 16:02:12 +0200

    fix: Use anonymous function instead of arrow function in userSchema pre save to allow for use of 'this'

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -20,7 +20,7 @@ const userSchema = mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre('save', async next => {
+userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
     next();
   }

492c9790749a2251d9070b58f91cb803b621d63e
Simen H <simenmelo@gmail.com>
2021-09-24 17:10:29 +0200

    feat: Add error handler middleware

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import router from './router.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 
 dotenv.config();
@@ -11,7 +12,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
 app.use(router);
+
+app.use(notFound);
+app.use(errorHandler);
 
 connectDB();
 app.listen(

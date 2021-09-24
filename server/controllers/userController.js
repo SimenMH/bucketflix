705897c6254d7a7a61e6ec4c50a359d62dec808e
Simen H <simenmelo@gmail.com>
2021-09-24 17:06:53 +0200

    feat: Add createUser route and controller

---
+++ server/controllers/userController.js
@@ -0,0 +1,29 @@
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+
+const createUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  const userExists = User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('Username or email already exists');
+  }
+
+  const user = await User.create({ username, email, password });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
+export { createUser };
--- server/router.js
+++ server/router.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import { createUser } from './controllers/userController.js';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('hello world');
 });
+
+router.post('/user', createUser);
 
 export default router;

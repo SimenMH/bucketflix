9e3e2a64678c2a9dcdba46c6197cf83033a3ef4a
Simen H <simenmelo@gmail.com>
2021-09-29 00:11:55 +0200

    feat: Rename .env JWT_SECRET variable to ACCESS_TOKEN_SECRET

--- server/utils/generateAccessToken.js
+++ server/utils/generateAccessToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const generateAccessToken = user => {
-  return jwt.sign(user, process.env.JWT_SECRET, {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
     expiresIn: '5m',
   });
 };
